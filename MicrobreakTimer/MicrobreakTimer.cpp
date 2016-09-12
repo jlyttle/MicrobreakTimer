e53821d40cd0258d740f7538c5d3903a9eb50a59
Jonathan Lyttle <myncijub@gmail.com>
2016-09-12 00:24:44 -0500

    Show a random message for every break

--- MicrobreakTimer/MicrobreakTimer.cpp
+++ MicrobreakTimer/MicrobreakTimer.cpp
@@ -3,6 +3,10 @@
 
 #include "stdafx.h"
 #include "MicrobreakTimer.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
@@ -14,10 +18,34 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
 	int messageBoxAlarm = IDOK;
 	
+	//int sleepTime = DialogBox(NULL, MAKEINTRESOURCE(ID_PASSWORD), NULL, DlgProc);
+	//ifstream friendFile("messages.txt");
+	//friendFile.open;
+	//string lines;
+	//int numberOfLines = 0;
+
+	//while (getline(friendFile, lines))
+	//{
+		//++numberOfLines;
+	//}
+
+	//int chosenLineNumber = rand() % numberOfLines;
+
+	//for (int i = 0; i < chosenLineNumber; ++i)
+		//getline(friendFile, lines);
+	//getline(friendFile, lines);
+
+	LPCWSTR messages[] = { L"You look really beautiful today. Why not give yourself a standing ovation?",
+						   L"Remember to take a break and stretch.",
+						   L"Your floor is feeling really lonely right now. Why not go visit the rest of it?",
+						   L"Sometimes it helps to clear your head by taking a short walk. Why not right now?" };
+
 	while (messageBoxAlarm == IDOK)
 	{
 		Sleep(1000);
-		messageBoxAlarm = MessageBox(NULL, L"Remember to take a break and stretch", L"Microbreak Reminder", MB_SETFOREGROUND | MB_ICONINFORMATION | MB_DEFBUTTON2 | MB_OKCANCEL);
+		int chosenLineNumber = rand() % 4;
+		LPCWSTR chosenLine = messages[chosenLineNumber];
+		messageBoxAlarm = MessageBox(NULL, chosenLine, L"Microbreak Timer", MB_SETFOREGROUND | MB_ICONINFORMATION | MB_DEFBUTTON2 | MB_OKCANCEL);
 	}
 	return 0;
 }
