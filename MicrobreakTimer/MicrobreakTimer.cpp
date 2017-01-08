9f4797a244771b9e97546376f1194b18ca5bf8d1
picoriley <acloudy@mail.smu.edu>
2017-01-07 20:38:46 -0600

    Added tray icon

--- MicrobreakTimer/MicrobreakTimer.cpp
+++ MicrobreakTimer/MicrobreakTimer.cpp
@@ -3,11 +3,55 @@
 
 #include "stdafx.h"
 #include "MicrobreakTimer.h"
+#include "shellapi.h"
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
+
+#define WM_MYMESSAGE (WM_APP + 1)
+
 using namespace std;
+
+void RunMessagePump()
+{
+    MSG queuedMessage;
+    for (;;)
+    {
+        const BOOL wasMessagePresent = PeekMessage(&queuedMessage, NULL, 0, 0, PM_REMOVE);
+        if (!wasMessagePresent)
+        {
+            break;
+        }
+
+        TranslateMessage(&queuedMessage);
+        DispatchMessage(&queuedMessage);
+    }
+}
+
+
+LRESULT CALLBACK WindowsMessageHandlingProcedure(HWND windowHandle, UINT wmMessageCode, WPARAM wParam, LPARAM lParam)
+{
+    unsigned char asKey = (unsigned char)wParam;
+    switch (wmMessageCode)
+    {
+    case WM_CREATE:
+        break;
+    case WM_MYMESSAGE:
+        switch (lParam)
+        {
+        case WM_LBUTTONDBLCLK:
+            MessageBox(NULL, L"Tray icon double clicked!", L"clicked", MB_OK);
+            break;
+        default:
+            return DefWindowProc(windowHandle, wmMessageCode, wParam, lParam);
+        };
+        break;
+    default:
+        return DefWindowProc(windowHandle, wmMessageCode, wParam, lParam);
+    }
+
+}
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
@@ -16,6 +60,18 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 {
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
+
+    NOTIFYICONDATA notificationData;
+    notificationData.cbSize = sizeof(NOTIFYICONDATA);
+    notificationData.hWnd = NULL;
+    notificationData.uID = 100;
+    notificationData.uVersion = NOTIFYICON_VERSION;
+    notificationData.uCallbackMessage = WM_MYMESSAGE;
+    notificationData.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wcscpy_s(notificationData.szTip, L"Tray Icon");
+    notificationData.uFlags = NIF_MESSAGE | NIF_ICON | NIF_TIP;
+
+    Shell_NotifyIcon(NIM_ADD, &notificationData);
 
 	int messageBoxAlarm = IDOK;
 	
